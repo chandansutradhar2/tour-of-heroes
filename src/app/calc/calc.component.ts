de12edd38993cb463b117081a01576316a404067
chandan <chandansutradhar2@gmail.com>
2022-10-28 14:51:35 +0530

    event binding demo with calc function

--- src/app/calc/calc.component.ts
+++ src/app/calc/calc.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalcComponent implements OnInit {
 
+  values:number[]=[];
   container:string="0";
 
-  constructor() { }
+  lastAction!:string;
+  constructor() {
+
+   }
+
+  add():number{
+    let res:number=0;
+    this.values.forEach(item=>{
+      res=res+item;
+    })
+
+    return res;
+  }
+
+  subtract(){
+
+  }
+
 
   ngOnInit(): void {
   }
@@ -18,12 +36,34 @@ export class CalcComponent implements OnInit {
     this.container+=0
   }
 
-  onOnePress(){
-    this.container+=1;
+  onNumpadPress(number:string){
+    this.container+=number;
   }
 
   onClear(){
+    this.values=[];
     this.container="0";
+  }
+
+  onAddPress(){
+    this.lastAction="add";
+    this.values.push(parseFloat(this.container));
+    this.container="0";
+    console.log(this.values);
+
+  }
+
+  onEqualClicked(){
+    this.values.push(parseFloat(this.container));
     
+    switch (this.lastAction) {
+      case "add":
+        this.container=this.add().toString();
+       console.log(this.container);
+        break;
+    
+      default:
+        break;
+    }
   }
 }
